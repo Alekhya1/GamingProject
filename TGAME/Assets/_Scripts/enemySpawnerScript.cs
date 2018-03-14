72d1f4cb2ff2d0553169d1ffc2d7bcf8374154c1
jibinsamreji <jibinsamreji@gmail.com>
2018-03-13 23:20:42 -0400

    First Playable Build-Dalek Game
    Tried Our best since we are new to game programming.

---
+++ TGAME/Assets/_Scripts/enemySpawnerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawnerScript : MonoBehaviour {
+    public GameObject enemy;
+    public Transform player;
+    float randX, randY;
+    Vector2 whereToSpawn;
+    public float spawnrate = 2f;
+    float nextSpawn = 0.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Time.time>nextSpawn)
+        {
+            nextSpawn = Time.time + spawnrate;
+            randX = Random.Range(-2.95f, 1.45f);
+            randY = Random.Range(-4.0f, 4.0f);
+            whereToSpawn = new Vector2(randX, randY);
+            Instantiate(enemy, whereToSpawn, Quaternion.identity); 
+        }
+		
+	}
+    
+}
